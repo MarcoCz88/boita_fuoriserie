613095bfe450042c06412cd3ebf82696ef854304
Marco Corradi <marcocorr2006@gmail.com>
2024-09-21 14:05:37 +0200

    aggiornata pagine servizi e progetti

--- assets/style/js/script.js
+++ assets/style/js/script.js
@@ -1,11 +1,60 @@
 // Chiudere la navbar cliccando fuori
-$(document).click(function(event) {
-    var clickover = $(event.target);
-    var _opened = $(".navbar-collapse").hasClass("show");
-    if (_opened === true && !clickover.hasClass("navbar-toggler")) {
-        $(".navbar-toggler").click();
-    }
-});
+$(document).ready(function() {
+    $(document).click(function(event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+  
+    // Inizializzare Swiper
+    const swiper = new Swiper('.swiper-container', {
+        // Configurazione Swiper
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+  });
+  //SERVIZI
+  document.querySelectorAll('.show-more-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const moreContent = button.previousElementSibling;
+        if (moreContent.style.display === 'none' || moreContent.style.display === '') {
+            moreContent.style.display = 'block';
+            button.textContent = 'Mostra di meno';
+        } else {
+            moreContent.style.display = 'none';
+            button.textContent = 'Mostra di più';
+        }
+    });
+  });
+  //PROGETTI
+  document.addEventListener('DOMContentLoaded', function() {
+    const dots = document.querySelectorAll('.dot');
+    const cards = document.querySelectorAll('.card-container');
 
-import 'swiper/swiper-bundle.css';
-import Swiper from 'swiper';
+    dots.forEach(dot => {
+        dot.addEventListener('click', function() {
+            const target = this.getAttribute('data-target');
+
+            cards.forEach(card => {
+                if (card.id === target) {
+                    card.classList.add('active');
+                } else {
+                    card.classList.remove('active');
+                }
+            });
+        });
+    });
+  });
