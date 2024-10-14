0495a268e435f8fb47b315ace7da14da6fb15b58
Marco Corradi <marcocorr2006@gmail.com>
2024-10-14 14:43:07 +0200

    modifiche

---
+++ assets/style/js/script.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+
+    //NAVBAR
+    $(document).click(function(event) {
+        if ($(".navbar-collapse").hasClass("show") && !$(event.target).closest(".navbar").length) {
+            $(".navbar-toggler").click();
+        }
+    });
+
+    
+    $('.navbar-nav .nav-link').click(function(event) {
+        if ($(".navbar-collapse").hasClass("show") && !$(event.target).closest('.dropdown').length) {
+            $(".navbar-toggler").click(); 
+        }
+    });
+
+    
+    $('.dropdown-menu a').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+   //SWIPER
+    if ($('.swiper-container').length) {
+        swiper = new Swiper('.swiper-container', {
+            direction: 'horizontal',
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+            effect: 'fade',
+        });
+    }
+
+    // SERVIZI
+    $('.show-more-btn').click(function() {
+        const moreContent = $(this).prev();
+        $('.show-more-content').not(moreContent).removeClass('expanded').next().text('Mostra di più');
+        moreContent.toggleClass('expanded');
+        $(this).text(moreContent.hasClass('expanded') ? 'Mostra di meno' : 'Mostra di più');
+    });
+
+    // PROGETTI
+    const dots = $('.dot');
+    const cards = $('.card-container');
+    if (cards.length) {
+        cards.first().addClass('active');
+        dots.first().addClass('active');
+
+        dots.click(function() {
+            const target = $(this).data('target');
+            cards.removeClass('active').filter(`#${target}`).addClass('active');
+            dots.removeClass('active').filter(this).addClass('active');
+        });
+
+        // Logica per le frecce delle card
+        const prevBtn = $('#prevBtn');
+        const nextBtn = $('#nextBtn');
+        if (prevBtn.length && nextBtn.length) {
+            let currentIndex = 0;
+
+            function showCard(index) {
+                cards.removeClass('active').eq(index).addClass('active');
+                dots.removeClass('active').eq(index).addClass('active');
+            }
+
+            prevBtn.click(() => {
+                currentIndex = (currentIndex === 0) ? cards.length - 1 : currentIndex - 1;
+                showCard(currentIndex);
+            });
+
+            nextBtn.click(() => {
+                currentIndex = (currentIndex === cards.length - 1) ? 0 : currentIndex + 1;
+                showCard(currentIndex);
+            });
+
+            showCard(currentIndex);
+        }
+    }
+});
+document.getElementById('contactButton').addEventListener('click', function() {
+    window.location.href = './contatti.html';
+});
